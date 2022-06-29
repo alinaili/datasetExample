40c7ed669b1581f8bd0dae4b97372ce12a9c0379
alinaili <60196863+alinaili@users.noreply.github.com>
2022-06-29 18:19:07 +0100

    Add files via upload

---
+++ dataset/script.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 29 10:29:25 2022
+
+@author: naili
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+datar = pd.read_csv('dataset.csv')
+plt.show()
+#datar.plot()
+#plt.legend()
+#plt.xticks(datar.nationality)
+#print(datar)
